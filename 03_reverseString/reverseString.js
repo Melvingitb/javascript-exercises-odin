38bd2148602349e7b304363ea3423631cdd7c117
Melvin <melvingit@gmail.com>
2023-08-20 20:26:54 -0700

    finish exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    answer = "";
 
+    for (let i = string.length - 1; i >= 0; i--){
+        answer += string.charAt(i);
+    }
+
+    return answer;
 };
 
 // Do not edit below this line
