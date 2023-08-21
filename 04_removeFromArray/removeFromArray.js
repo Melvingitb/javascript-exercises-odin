89a7e56d8be800234c53403903b9ef1a4a82d958
Melvin <melvingit@gmail.com>
2023-08-20 22:32:04 -0700

    finish exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...rest) {
+    let count = 0;
+    let answer = [];
 
+    for (let i = 0; i < arr.length; i++){
+        if (!rest.includes(arr[i])){
+            answer[count] = arr[i];
+            count++;
+        }
+    }
+
+    return answer;
 };
 
 // Do not edit below this line
